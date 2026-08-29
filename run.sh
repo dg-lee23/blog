9c7c68e3f7b32c914c4f72cad09a3739faefe61d
Donggyu Lee <47566711+dg-lee23@users.noreply.github.com>
2026-08-29 07:34:07 +0000

    add /utils to gitignore

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-hugo server -D --bind 0.0.0.0 --baseURL "https://$CODESPACE_NAME-1313.app.github.dev/" --appendPort=false
-
 hugo server -D --bind 0.0.0.0 --port 1314 --baseURL "https://$CODESPACE_NAME-1314.app.github.dev/" --appendPort=false
