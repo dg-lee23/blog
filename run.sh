5c5974aa95d78169dbe52a50ecf013a35fd023c6
Donggyu Lee <47566711+dg-lee23@users.noreply.github.com>
2026-08-29 05:59:06 +0000

    added command for port 1314

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 hugo server -D --bind 0.0.0.0 --baseURL "https://$CODESPACE_NAME-1313.app.github.dev/" --appendPort=false
+
+hugo server -D --bind 0.0.0.0 --port 1314 --baseURL "https://$CODESPACE_NAME-1314.app.github.dev/" --appendPort=false
